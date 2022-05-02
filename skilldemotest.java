1996b4834b86cb1a4b5ec96243ccf074f7df0dde
mijinson <103203170+mijinson@users.noreply.github.com>
2022-05-01 23:31:57 -0700

    initial file set up

---
+++ skilldemotest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class skilldemotest {
+    @Test
+    public void testSearch() {
+        assertTrue(skilldemo.searchFor(4));
+    }
+}
